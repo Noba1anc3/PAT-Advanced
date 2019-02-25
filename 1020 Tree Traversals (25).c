f8aa5a57b29c513477f83b190a62aaff1648ab11
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-02-25 16:50:59 +0800

    Update 1020 Tree Traversals (25).c

--- 1020 Tree Traversals (25).c
+++ 1020 Tree Traversals (25).c
@@ -1,33 +1,35 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct BinTNode{
-	char data;
+	int data;
 	struct BinTNode *lchild, *rchild;
 }BinTNode, *BinTree;
 
-BinTree ReBuildTree(char *post, int i, int j, char *in, int m, int n);
+BinTree ReBuildTree(int *post, int i, int j, int *in, int m, int n);
 void LevelOrderTraversal(BinTree BT, int n);
 
 int main()
 {
 	int n, i;
 	BinTree p;
-	char post[300], in[300];
+	int post[30], in[30];
+
 	scanf("%d", &n);
 	for(i = 0;i < n;i++)
-		scanf("%c ", &post[i]);
-	scanf("%c", &post[i]);
+		scanf("%d", &post[i]);
 	for(i = 0;i < n;i++)
-		scanf("%c ", &in[i]);
-	scanf("%c", &in[i]);
-	p = ReBuildTree(post,1,n,in,1,n);
+		scanf("%d", &in[i]);
+
+	p = ReBuildTree(post, 0, n-1, in, 0, n-1);
 	LevelOrderTraversal(p, n);
+
     return 0;
 }
 
 
-BinTree ReBuildTree(char *post, int i, int j, char *in, int m, int n)
+BinTree ReBuildTree(int *post, int i, int j, int *in, int m, int n)
 {
     //post[i.....j] in[m....n]
     int x;  //x是遍历器
@@ -37,53 +39,47 @@ BinTree ReBuildTree(char *post, int i, int j, char *in, int m, int n)
     x = m;
 
 	//在中序中找到根节点位置
-    for (; *(in + x) != *(post + j); x++)
+    for (; *(in + x) != p->data; x++)
 		;
 
     //接下来递归完成左右子树的构建
     if (x == m) 
 		p->lchild = NULL;
     else
-    {
         p->lchild = ReBuildTree(post, i, i+x-m-1, in, m, x-1);
-    }
 
     if (x == n) 
 		p->rchild = NULL;
-
     else
-    {
         p->rchild = ReBuildTree(post, i+x-m, j-1, in, x+1,n);
-    } 
+ 
     return p;
 }
 
 void LevelOrderTraversal(BinTree BT, int n){
 
-	BinTree queue[300], T;
-	int head = 0, rear = 0, count;
+	BinTree queue[30], T;
+	int head = 0, rear = 0, count = 0;
 
 	if(!BT)
 		return;
 	
-	queue[head] = BT;
-	rear++;
+	queue[rear++] = BT;
 	
 	while(head != rear){
 		T = queue[head++];
-	  if(count < n-1){
-  		printf("%d ", T->data - 48);
-  		count++;
-	  }
-  	else
-  		printf("%d", T->data - 48);
-		if(T->lchild){
-			queue[rear] = T->lchild;
-			rear++;
+		if(count < n-1){
+  			printf("%d ", T->data);
+  			count++;
 		}
-		if(T->rchild){
-			queue[rear] = T->rchild;
-			rear++;
-		}
+		else
+  			printf("%d\n", T->data);
+
+		if(T->lchild)
+			queue[rear++] = T->lchild;
+		
+		if(T->rchild)
+			queue[rear++] = T->rchild;
+		
 	}
 }
