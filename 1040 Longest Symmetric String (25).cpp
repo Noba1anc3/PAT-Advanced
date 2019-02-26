d41fa54d72125409c73015a9e2a39e8c72454222
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-02-26 19:34:06 +0800

    Update 1040 Longest Symmetric String (25).cpp

--- 1040 Longest Symmetric String (25).cpp
+++ 1040 Longest Symmetric String (25).cpp
@@ -1,5 +1,4 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
@@ -10,17 +9,14 @@ int main(){
 
     for(int i = 0; i < s.size(); i++){
         int j;
-
         for(j = 1; i - j >= 0 && i + j < s.size() && s[i+j] == s[i-j]; j++)//以当前字符为回文中心查找最长回文子串 
-			;
+		;
 		maxLen = max(maxLen, 2*j-1);//更新回文子串最大长度
-
         for(j = 0; i - j >= 0 && i + j + 1 < s.size() && s[i+j+1] == s[i-j]; j++)//以当前字符为回文中心左侧字符查找最长回文子串
-			;
+		;
         maxLen=max(maxLen, 2*j);//更新回文子串最大长度
     }
 
     printf("%d",maxLen);
-
     return 0;
 }
