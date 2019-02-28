eecca167c3e2bdef3cfa0ced9d68e409dfe13444
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-02-28 16:44:11 +0800

    Update 1149 Dangerous Goods Packaging (25).cpp

--- 1149 Dangerous Goods Packaging (25).cpp
+++ 1149 Dangerous Goods Packaging (25).cpp
@@ -6,33 +6,28 @@ using namespace std;
 int main() {
     int n, k, t1, t2;
     map<int, vector<int>> m;
-
-	cin>>n>>k;
+    cin>>n>>k;
     for (int i = 0; i < n; i++) {
-		cin>>t1>>t2;
+	cin>>t1>>t2;
         m[t1].push_back(t2);
         m[t2].push_back(t1);
     }
 
     while(k--){
         int num, flag = 0, a[100000] = {0};
-
         cin>>num;
         vector<int> v(num);
-
         for (int i = 0; i < num; i++) {
             scanf("%d", &v[i]);
             a[v[i]] = 1;
         }
-
         for (int i = 0; i < v.size(); i++)
             for (int j = 0; j < m[v[i]].size(); j++)
                 if (a[m[v[i]][j]] == 1) 
-					flag = 1;
-
-		printf("%s\n",flag ? "No" : "Yes");
+			flag = 1;
+	printf("%s\n",flag ? "No" : "Yes");
     }
-
+	
     return 0;
 }
 
