d9517ef05a4a4d815e0009a2d60f7c23435f39e8
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-03-13 16:43:57 +0800

    Create BUAA_2016_01.cpp

---
+++ BUAA_2016_01.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    int tmp;
+    char str1[100];
+    char str2[100];
+    scanf("%d",&n1);
+    itoa(n1,str1,10);
+    reverse(str1,str1+strlen(str1));
+    n2=atoi(str1);
+    if(n2%n1==0)
+        printf("%d*%d=%s",n1,n2/n1,str1);
+	else
+        printf("%d %s",n1,str1);
+
+    return 0;
+}
